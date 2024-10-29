4237fd23ccd990b66ed8c14a0bc63ba36028cc96
DhayananthG <iamdhayaoffl@gmail.com>
2024-10-29 06:45:28 +0530

    Completed All Except EmployeeList

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const PORT = 8000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 DBConnection();
 
 
@@ -19,70 +20,84 @@ app.get('/employees', async (req, res, next) => {
     } 
     catch (error) 
     {
-        res.status(500).json({ error: 'Error fetching employees' });  
+        res.status(404).json({ error: 'Error fetching employees' });  
     }
 });
 
 
+// To find the Employee by ID [ Working ]
+app.get('/searchEmp/:id', async (req, res) => {
+  try 
+  {
+    const Eid = req.params.id;
+    const employee = await Employee.findOne({ EmployeeID : Eid });
+      if (!employee) 
+      {
+          return res.status(404).json({ message: "Employee not found" });
+      }
+      res.status(200).json(employee);
+  } 
+  catch (error) 
+  {
+      res.status(500).json({ error: "An error occurred while searching for the employee." });
+  }
+});
+
+
 // To Add a new employee data into the database [ Working ]
-app.post('/employee', async (req, res, next) => {
-    try {
-        const newEmployee = new Employee(req.body); 
+app.post('/addEmployee', async (req, res, next) => {
+    const newEmployee = new Employee(req.body); 
+    try 
+    {
         await newEmployee.save();
         res.status(201).json({ message: "Employee added successfully" });
     } 
     catch (error) {
         console.error('Error adding employee:', error);
-        res.status(500).json({ error: 'Error adding employee to the database' });
+        res.status(404).json({ error: 'Error adding employee to the database' });
     }
 });
 
 
-// To find the Employee by ID 
-app.post('/searchEmployee', async (req, res) => {
+// To update an employee [ Working ]
+app.put('/updateEmp/:id', async (req, res) => {
     try {
-        const { EmployeeID } = req.body;
-
-        // Find employee by EmployeeID
-        const employee = await Employee.findOne({ EmployeeId: EmployeeID });
-        
-        if (!employee) {
-            return res.status(404).json({ message: "Employee not found" });
-        }
-
-        res.status(200).json(employee);
-    } 
-    catch (error) {
-        res.status(500).json({ error: "An error occurred while searching for the employee." });
+      const Eid = req.params.id;
+      const data = req.body;
+      const updatedEmployee = await Employee.findOneAndUpdate({EmployeeID: Eid}, data, {new : true});
+  
+      if (!updatedEmployee) {
+        return res.status(404).json({ message: 'Employee not found' });
+      }
+  
+      res.status(201).json({ message: 'Designation updated successfully', employee: updatedEmployee });
+    } catch (error) {
+      res.status(500).json({ message: 'Error updating designation', error });
     }
-});
+  });
 
 
-// To update an employee 
-app.put('/updateEmployee', async (req, res) => {
+// To Delete an Employee [ Working ]
+app.delete('/deleteEmp/:id', async (req, res) => {
     try 
     {
-        const { EmployeeID, newDesignation } = req.body;
-        const updatedEmployee = await Employee.findOneAndUpdate(
-            { employeeId: EmployeeID },
-            { designation: newDesignation },
-            { new: true }
-        );
-
-        if (!updatedEmployee) {
-            return res.status(404).json({ message: "Employee not found" });
+        const Eid = req.params.id;
+        const deletedEmployee = await Employee.findOneAndDelete(Eid);
+      
+        if (!deletedEmployee) 
+        {
+            return res.status(404).json({ message: 'Employee not found' });
         }
-
-        res.status(200).json({ message: "Designation updated successfully", employee: updatedEmployee });
+        res.status(201).json({ message: 'Employee deleted successfully', employee: deletedEmployee });
     } 
     catch (error) 
     {
-        res.status(500).json({ error: "An error occurred while updating the designation." });
+        res.status(500).json({ message: 'Error deleting employee', error });
     }
-});
-
+  });
+  
   
 
 app.listen(PORT, ()=> {
     console.log(`\nServer is Running on Port ${PORT}`); 
-})
+});
